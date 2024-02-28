7e89c142beeac474f077e83299c8459df53ea637
Peter <pmitrano@umich.edu>
2024-02-28 14:23:46 -0500

    add conversions between Transform and numpy
    with tests

--- arm_utilities/numpy_conversions.py
+++ arm_utilities/numpy_conversions.py
@@ -1,19 +1,36 @@
 import numpy as np
 
-def ListPointsToNpArray(points, transform=None):
-    """
-    :param points: Anything that is itterable, with each element having x, y, and z elements
-    :type points: :class:`list[geometry_msgs.msgs.Point]`
-    :param transform:
-    :type transform: :class:`np.array`
-    :return:
-    """
-    arr = np.empty(shape=(3, len(points)))
-    if transform is not None:
-        for ind in range(len(points)):
-            point = transform.dot([points[ind].x, points[ind].y, points[ind].z, 1])
-            arr[:, ind] = point[0:3]
-    else:
-        for ind in range(len(points)):
-            arr[:, ind] = [points[ind].x, points[ind].y, points[ind].z]
-    return arr
+from geometry_msgs.msg import Transform, Quaternion
+
+
+def quat_to_rot_mat(q: Quaternion):
+    """ Convert a geometry_msgs/Quaternion to a 3x3 numpy array """
+    x, y, z, w = q.x, q.y, q.z, q.w
+    return np.array([[1 - 2 * y ** 2 - 2 * z ** 2, 2 * x * y - 2 * z * w, 2 * x * z + 2 * y * w],
+                     [2 * x * y + 2 * z * w, 1 - 2 * x ** 2 - 2 * z ** 2, 2 * y * z - 2 * x * w],
+                     [2 * x * z - 2 * y * w, 2 * y * z + 2 * x * w, 1 - 2 * x ** 2 - 2 * y ** 2]])
+
+
+def rot_mat_to_quat(mat: np.ndarray):
+    """ Convert a 3x3 numpy array to a geometry_msgs/Quaternion """
+    w = np.sqrt(1 + mat[0, 0] + mat[1, 1] + mat[2, 2]) / 2
+    x = (mat[2, 1] - mat[1, 2]) / (4 * w)
+    y = (mat[0, 2] - mat[2, 0]) / (4 * w)
+    z = (mat[1, 0] - mat[0, 1]) / (4 * w)
+    return Quaternion(x=x, y=y, z=z, w=w)
+
+
+def transform_to_mat(transform: Transform):
+    """ Convert a geometry_msgs/Transform to a 4x4 numpy array """
+    mat = np.eye(4)
+    mat[:3, 3] = [transform.translation.x, transform.translation.y, transform.translation.z]
+    mat[:3, :3] = quat_to_rot_mat(transform.rotation)
+    return mat
+
+
+def mat_to_transform(mat: np.ndarray):
+    """ Convert a 4x4 numpy array to a geometry_msgs/Transform """
+    t = Transform()
+    t.translation.x, t.translation.y, t.translation.z = mat[:3, 3]
+    t.rotation = rot_mat_to_quat(mat[:3, :3])
+    return t
---
+++ tests/test_numpy_conversions.py
@@ -0,0 +1,49 @@
+from arm_utilities.numpy_conversions import quat_to_rot_mat, rot_mat_to_quat, transform_to_mat, mat_to_transform
+
+import numpy as np
+
+from geometry_msgs.msg import Transform, Quaternion
+
+
+def test_quat_to_rot_mat():
+    q = Quaternion()
+    q.x = 0.5
+    q.y = 0.5
+    q.z = 0.5
+    q.w = 0.5
+    expected = np.array([[0, 0, 1], [1, 0, 0], [0, 1, 0]], dtype=float)
+    assert np.allclose(quat_to_rot_mat(q), expected)
+
+
+def test_rot_mat_to_quat():
+    mat = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]], dtype=float)
+    expected = np.array([0., 0., 0.70710678, 0.70710678])
+    q = rot_mat_to_quat(mat)
+    assert np.allclose([q.x, q.y, q.z, q.w], expected)
+def test_transform_to_mat():
+    t = Transform()
+    t.translation.x = 1.
+    t.translation.y = 2.
+    t.translation.z = 3.
+    t.rotation.x = 0.
+    t.rotation.y = 1.
+    t.rotation.z = 0.
+    t.rotation.w = 0.
+    expected = np.array([[-1, 0, 0, 1], [0, 1, 0, 2], [0, 0, -1, 3], [0, 0, 0, 1]], dtype=float)
+    assert np.allclose(transform_to_mat(t), expected)
+
+
+def test_mat_to_transform():
+    mat = np.array([[0, -1, 0, 1], [1, 0, 0, 2], [0, 0, 1, 3], [0, 0, 0, 1]], dtype=float)
+    expected = np.array([1., 2., 3., 0., 0., 0.70710678, 0.70710678], dtype=float)
+    t = mat_to_transform(mat)
+    assert np.allclose([t.translation.x, t.translation.y, t.translation.z, t.rotation.x, t.rotation.y, t.rotation.z, t.rotation.w], expected)
+
+def test_to_and_from_rot_mat():
+    rot_mat = np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]], dtype=float)
+    assert np.allclose(quat_to_rot_mat(rot_mat_to_quat(rot_mat)), rot_mat)
+
+
+def test_to_and_from_mat():
+    mat = np.array([[0, -1, 0, 1], [1, 0, 0, 2], [0, 0, 1, 3], [0, 0, 0, 1]], dtype=float)
+    assert np.allclose(transform_to_mat(mat_to_transform(mat)), mat)
